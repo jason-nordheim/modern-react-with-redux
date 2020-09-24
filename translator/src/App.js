825acdf8ad35b90a4ace2a779141410fabd15954
Jason Nordheim <jason.nordheim@gmail.com>
2020-09-23 19:35:50 -0600

    added new component as a component within the App component

--- translator/src/App.js
+++ translator/src/App.js
@@ -1,5 +1,8 @@
 
 import React, { useState } from 'react'
+import Field from './components/Field'
+import Translate from './components/Translate'
+import Langauges from './components/Langauges'
 
 export default function App() {
     const [langauge, setLanguage] = useState('ru') // default to russian 
@@ -7,9 +10,9 @@ export default function App() {
 
     return (
         <div className="app">
-            {/* field to display text */}
-            {/* dropdown to display menus */}
-            {/* translated text  */}
+            <Field label="English" onChange={setText} value={text} /> 
+            <Langauges language={langauge} onLanguageChange={}/> 
+            <Translate langauge={langauge} text={text} /> 
         </div>
     )
 }
